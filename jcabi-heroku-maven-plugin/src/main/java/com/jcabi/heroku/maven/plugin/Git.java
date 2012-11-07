b595b1c13cd3ba486dd777953b01240e259e55dc
Yegor Bugayenko <yegor@tpc2.com>
2012-11-07 19:19:19 +0100

    #79: removed invalid SSH flag

--- jcabi-heroku-maven-plugin/src/main/java/com/jcabi/heroku/maven/plugin/Git.java
+++ jcabi-heroku-maven-plugin/src/main/java/com/jcabi/heroku/maven/plugin/Git.java
@@ -85,7 +85,7 @@ final class Git {
             this.script,
             String.format(
                 // @checkstyle LineLength (1 line)
-                "set -x && %s -n -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i '%s' $@",
+                "set -x && %s -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i '%s' $@",
                 Git.SSH,
                 kfile.getAbsolutePath()
             )
