8857a98a7e8b541629c28ecf5b3dfbf4e09f2ab0
Yegor Bugayenko <yegor@tpc2.com>
2013-07-14 08:08:44 +0200

    #264 removed unused import

--- jcabi-log/src/main/java/com/jcabi/log/MulticolorLayout.java
+++ jcabi-log/src/main/java/com/jcabi/log/MulticolorLayout.java
@@ -38,7 +38,6 @@ import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import org.apache.log4j.EnhancedPatternLayout;
 import org.apache.log4j.Level;
-import org.apache.log4j.PatternLayout;
 import org.apache.log4j.spi.LoggingEvent;
 
 /**
